b563aa0d3e81ac82561b51007f239b7c693b1f0f
Najma <syednajma82@gmail.com>
2025-09-22 13:24:50 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",                  // all HTML files in root
+    "./src/**/*.{js,jsx,ts,tsx}" // all JS/JSX/TS files in src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
